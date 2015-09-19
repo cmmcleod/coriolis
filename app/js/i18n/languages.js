d92722f1c26b4a4b8d686d331601c3daea9f4bdf
enrico <enricofriede@gmail.com>
2015-09-20 01:13:07 +0200

    added italian language

--- app/js/i18n/languages.js
+++ app/js/i18n/languages.js
@@ -3,9 +3,10 @@ angular.module('app').config(['$translateProvider', function($translateProvider)
     .useSanitizeValueStrategy('escapeParameters')
     .useStorage('Persist')
     .fallbackLanguage('en')   // Use English as default/fallback language
-    .registerAvailableLanguageKeys(['en', 'de', 'fr', 'ru'], {  // TODO: add 'es' to the array when ready
+    .registerAvailableLanguageKeys(['en', 'de', 'it', 'fr', 'ru'], {  // TODO: add 'es' to the array when ready
       'en*': 'en',
       'de*': 'de',
+      'it*': 'it',
       //'es*': 'es',
       'fr*': 'fr',
       'ru*': 'ru'
@@ -15,6 +16,7 @@ angular.module('app').config(['$translateProvider', function($translateProvider)
 .value('Languages', {
   en: 'English',
   de: 'Deutsh',
+  it: 'Italiano',
   //es: 'Español',
   fr: 'Français',
   ru: 'ру́сский'
