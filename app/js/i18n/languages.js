e16ce83c03c940c24da365da8f540e40881a432c
Colin McLeod <colin@roaminsight.com>
2015-09-21 23:44:05 -0700

    Adding Spanish support

--- app/js/i18n/languages.js
+++ app/js/i18n/languages.js
@@ -3,12 +3,12 @@ angular.module('app').config(['$translateProvider', function($translateProvider)
     .useSanitizeValueStrategy('escapeParameters')
     .useStorage('Persist')
     .fallbackLanguage('en')   // Use English as default/fallback language
-    .registerAvailableLanguageKeys(['en', 'de', 'it', 'fr', 'ru'], {  // TODO: add 'es' to the array when ready
+    .registerAvailableLanguageKeys(['en', 'de', 'es', 'fr', 'it', 'ru'], {
       'en*': 'en',
       'de*': 'de',
-      'it*': 'it',
-      //'es*': 'es',
+      'es*': 'es',
       'fr*': 'fr',
+      'it*': 'it',
       'ru*': 'ru'
     })
     .determinePreferredLanguage();
@@ -17,7 +17,7 @@ angular.module('app').config(['$translateProvider', function($translateProvider)
   en: 'English',
   de: 'Deutsh',
   it: 'Italiano',
-  //es: 'Español',
+  es: 'Español',
   fr: 'Français',
   ru: 'ру́сский'
 });
