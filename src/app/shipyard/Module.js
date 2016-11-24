294fadf7cd3144375e26942ca59a725aa0621584
Cmdr McDonald <Cmdr.McDonald@gmail.com>
2016-11-24 12:50:33 +0000

    Checkpoint - handle non-numeric modifiers

--- src/app/shipyard/Module.js
+++ src/app/shipyard/Module.js
@@ -28,7 +28,7 @@ export default class Module {
   /**
    * Get a value for a given modification
    * @param {Number} name  The name of the modification
-   * @return {Number}      The value of the modification, as an integer value scaled so that 1.23% == 123
+   * @return {object}      The value of the modification. If it is a numeric value then it is returned as an integer value scaled so that 1.23% == 123
    */
   getModValue(name) {
     return this.mods  && this.mods[name] ? this.mods[name] : null;
@@ -37,7 +37,7 @@ export default class Module {
   /**
    * Set a value for a given modification ID
    * @param {Number} name   The name of the modification
-   * @param {Number} value  The value of the modification, as an integer scaled so that -2.34% == -234
+   * @param {object} value  The value of the modification. If it is a numeric value then it should be an integer scaled so that -2.34% == -234
    */
   setModValue(name, value) {
     if (!this.mods) {
@@ -47,8 +47,12 @@ export default class Module {
     if (value == null || value == 0) {
       delete this.mods[name];
     } else {
-      // Round just to be sure
-      this.mods[name] = Math.round(value);
+      if (isNaN(value)) {
+        this.mods[name] = value;
+      } else {
+        // Round just to be sure
+        this.mods[name] = Math.round(value);
+      }
     }
   }
 
@@ -546,6 +550,6 @@ export default class Module {
    * @return {string} the damage types for this module; any combination of E T and K
    */
   getDamageType() {
-    return this.type;
+    return this.getModValue('type') || this.type;
   }
 }
