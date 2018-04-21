e391b563fbf4ddf7fefeb2f4e3759f054a6f675e
willyb321 <wbwilliam7@gmail.com>
2018-04-21 11:03:23 +1000

    change preset rolls to 100%, 75%, 50% and random

--- src/app/utils/BlueprintFunctions.js
+++ src/app/utils/BlueprintFunctions.js
@@ -253,57 +253,19 @@ export function getBlueprint(name, module) {
  * Provide 'worst' primary modifications
  * @param {Object}      ship      The ship for which to perform the modifications
  * @param {Object}      m         The module for which to perform the modifications
+ * @param {Number} percent The percent to set values to of full.
  */
-export function setWorst(ship, m) {
+export function setPercent(ship, m, percent) {
   ship.clearModifications(m);
   const features = m.blueprint.grades[m.blueprint.grade].features;
   for (const featureName in features) {
     const value = features[featureName][0];
-    _setValue(ship, m, featureName, value);
-  }
-}
-
-/**
- * Provide 'best' primary modifications
- * @param {Object}      ship      The ship for which to perform the modifications
- * @param {Object}      m         The module for which to perform the modifications
- */
-export function setBest(ship, m) {
-  ship.clearModifications(m);
-  const features = m.blueprint.grades[m.blueprint.grade].features;
-  for (const featureName in features) {
-    const value = features[featureName][1];
-    _setValue(ship, m, featureName, value);
-  }
-}
-
-/**
- * Provide 'extreme' primary modifications
- * @param {Object}      ship      The ship for which to perform the modifications
- * @param {Object}      m         The module for which to perform the modifications
- */
-export function setExtreme(ship, m) {
-  ship.clearModifications(m);
-  const features = m.blueprint.grades[m.blueprint.grade].features;
-  for (const featureName in features) {
-    let value;
-    if (Modifications.modifications[featureName].higherbetter) {
-      // Higher is better, but is this making it better or worse?
-      if (features[featureName][0] < 0 || (features[featureName][0] === 0 && features[featureName][1] < 0)) {
-        value = features[featureName][0];
-      } else {
-        value = features[featureName][1];
-      }
+    const featureIsBeneficial = isBeneficial(featureName, features[featureName]);
+    if (featureIsBeneficial === true) {
+      _setValue(ship, m, featureName, (percent / 100) * value);
     } else {
-      // Higher is worse, but is this making it better or worse?
-      if (features[featureName][0] < 0 || (features[featureName][0] === 0 && features[featureName][1] < 0)) {
-        value = features[featureName][1];
-      } else {
-        value = features[featureName][0];
-      }
+      _setValue(ship, m, featureName, value);
     }
-
-    _setValue(ship, m, featureName, value);
   }
 }
 
